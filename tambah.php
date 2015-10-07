9f6d70c04e88c3e9121c0d06c8bca27226190613
ArifatulFaizah <arifatul.faizah@mail.ugm.ac.id>
2015-10-07 17:11:36 +0700

    Create tambah.php

---
+++ tambah.php
@@ -0,0 +1,31 @@
+<html>
+<body>
+	
+<a href="view_mahasiswa.php">Lihat Mahasiswa</a>
+
+<form action="mhs.php">
+	<br><table border="1">
+		<tr>
+			<td>NIM</td>
+			<td><input type="text" name="nim" id="nim"></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td><input type="text" name="nama" id="nama"></td>
+		</tr>
+		<tr>
+			<td>Alamat</td>
+			<td><input type="text" name="alamat" id="alamat"></td>
+		</tr>
+		<tr>
+			<td>Prodi</td>
+			<td><input type="text" name="prodi" id="prodi"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="submit" value="tambah"></td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
+
